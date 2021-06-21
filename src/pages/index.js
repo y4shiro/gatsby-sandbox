8a87371491cecacf51b22caa77190b086338e0c5
y4shiro <y4shironao@gmail.com>
2021-06-21 18:22:23 +0900

    TypeScript インストール

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const IndexPage = () => {
   return (
     <main>
-      <h1 class="text-red-300">こんにちは、こんにちは</h1>
+      <h1 className="text-red-300">こんにちは、こんにちは</h1>
       <p>
         文章だよ 文章だよ 文章だよ 文章だよ 文章だよ 文章だよ 文章だよ 文章だよ
       </p>

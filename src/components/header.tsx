df4255fd9ceb1e8b93fde66694043693963f6542
y4shiro <y4shironao@gmail.com>
2021-06-22 21:09:17 +0900

    tailwind にカスタムカラー追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="p-4 bg-gray-200">
-      <h1>Header です</h1>
+    <header className="flex p-4 bg-sky-600 justify-between items-center text-gray-100">
+      <h1 className="text-2x">Header です</h1>
+      <ul className="flex">
+        <li>
+          <a href="#" className="block hover:bg-sky-300">
+            <span>About</span>
+          </a>
+        </li>
+        <li>Skills</li>
+        <li>Works</li>
+        <li>Contact</li>
+      </ul>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: colors.lightBlue,
+        sky: colors.sky,
+      },
+    },
   },
   variants: {
     extend: {},
